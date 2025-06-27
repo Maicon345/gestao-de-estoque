09bb8689a761c092361366d016d1ec36374c4298
Maicon345 <maiconsss16@gmail.com>
2025-06-27 19:11:06 -0300

    primeira versao

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="estilos.css">
+    <title>Estoque do Mercado</title>
+</head>
+<body>
+    <h1>📦 Controle de Estoque</h1>
+    <a href="cadastrar_produto.php">➕ Cadastrar Produto</a>
+    <a href="movimentar_estoque.php">🔁 Mecher Estoque</a>
+    <a href="visualizar_estoque.php">📊 Visualizar Estoque</a>
+</body>
+</html>
